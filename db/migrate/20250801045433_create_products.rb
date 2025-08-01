7e6c337ca6ec95fb536841fae2a95deaa1edb4d9
연지 조 <mitaetoo@yeonjiui-MacBookAir.local>
2025-08-01 23:17:18 +0900

    🔧 Remove convenience_store reference from products migration to fix order issue

--- db/migrate/20250801045433_create_products.rb
+++ db/migrate/20250801045433_create_products.rb
@@ -4,7 +4,6 @@ class CreateProducts < ActiveRecord::Migration[7.2]
       t.string :name
       t.string :price
       t.boolean :is_official
-      t.references :convenience_store, null: false, foreign_key: true
 
       t.timestamps
     end
