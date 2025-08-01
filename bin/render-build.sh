817d6b5d155f3e2f3076848a0cb747eaabe002db
연지 조 <mitaetoo@yeonjiui-MacBookAir.local>
2025-08-01 23:01:47 +0900

    🔧 Force npm usage for Render deployment - Add stronger configuration

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,13 @@
 set -o errexit
 
-# Set package managers to npm
+# Set package managers to npm and disable yarn
 export JSBUNDLING_PACKAGE_MANAGER=npm
 export CSSBUNDLING_PACKAGE_MANAGER=npm
+export YARN_IGNORE_PATH=1
+export YARN_IGNORE_ENGINES=1
+
+# Remove yarn.lock if it exists to force npm
+rm -f yarn.lock
 
 bundle install
 npm install
